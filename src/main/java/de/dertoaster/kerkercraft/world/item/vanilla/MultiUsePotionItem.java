8d41cc42b69d026e757d4b11f2d983a5bc3f77fc
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2025-06-09 13:25:37 +0200

    add actual class for multi use potion

---
+++ src/main/java/de/dertoaster/kerkercraft/world/item/vanilla/MultiUsePotionItem.java
@@ -0,0 +1,32 @@
+package de.dertoaster.kerkercraft.world.item.vanilla;
+
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.PotionItem;
+import net.minecraft.world.level.Level;
+
+public class MultiUsePotionItem extends PotionItem {
+    public MultiUsePotionItem(Properties p_42979_) {
+        super(p_42979_);
+    }
+
+    @Override
+    public boolean isBarVisible(ItemStack stack) {
+        // we always want the bar to be visible
+        return true;
+    }
+
+    @Override
+    public ItemStack finishUsingItem(ItemStack stack, Level level, LivingEntity livingEntity) {
+        return super.finishUsingItem(stack, level, livingEntity);
+    }
+
+    // There is custom logic for waterbottles and converting mud, that should stay as it is!
+
+
+    @Override
+    public int getBarColor(ItemStack stack) {
+        // Color: Dark blue
+        return 0x8195ce;
+    }
+}
