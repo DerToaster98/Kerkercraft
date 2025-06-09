2a4bc053e4125e55c8490908e91e7c0e079a5505
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2025-06-09 19:49:38 +0200

    create armor materials and equipment assets

--- src/main/java/de/dertoaster/kerkercraft/init/KCArmorMaterials.java
+++ src/main/java/de/dertoaster/kerkercraft/init/KCArmorMaterials.java
@@ -1,2 +1,52 @@
-package de.dertoaster.kerkercraft.init;public class KCArmorMaterials {
+package de.dertoaster.kerkercraft.init;
+
+import com.google.common.collect.Maps;
+import net.minecraft.core.Holder;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.tags.ItemTags;
+import net.minecraft.world.item.equipment.*;
+
+import java.util.Map;
+
+public interface KCArmorMaterials extends ArmorMaterials {
+
+    public static final ArmorMaterial HEAVY_IRON = createHeavy(ArmorMaterials.IRON, KCEquipmentAssets.HEAVY_IRON);
+    public static final ArmorMaterial HEAVY_DIAMOND = createHeavy(ArmorMaterials.DIAMOND, KCEquipmentAssets.HEAVY_DIAMOND);
+    public static final ArmorMaterial HEAVY_NETHERITE = createHeavy(ArmorMaterials.NETHERITE, KCEquipmentAssets.HEAVY_NETHERITE);
+
+    public static final ArmorMaterial TORTOISE = new ArmorMaterial(20, ArmorMaterials.makeDefense(3, 5, 7, 3, 8), 8, SoundEvents.ARMOR_EQUIP_TURTLE, 2.0F, 0.3F, KCItemTags.REPAIRS_TORTOISE_ARMOR, KCEquipmentAssets.TORTOISE);
+
+    public static final ArmorMaterial BULL = new ArmorMaterial(17, ArmorMaterials.makeDefense(2, 4, 8, 4, 7), 10, SoundEvents.ARMOR_EQUIP_NETHERITE, 0.0F, 0.05F, KCItemTags.REPAIRS_BULL_ARMOR, KCEquipmentAssets.BULL);
+
+    // TODO: Create own sound and repair material tag!
+    public static final ArmorMaterial SLIME = new ArmorMaterial(15, ArmorMaterials.makeDefense(2, 5, 6, 2, 5), 15, Holder.direct(SoundEvents.SLIME_BLOCK_PLACE), 0.0F, 0.025F, KCItemTags.REPAIRS_SLIME_ARMOR, KCEquipmentAssets.SLIME);
+
+    public static final ArmorMaterial DYABLE_GOLD = createDyable(ArmorMaterials.GOLD, KCEquipmentAssets.DYABLE_GOLD);
+    public static final ArmorMaterial DYABLE_IRON = createDyable(ArmorMaterials.IRON, KCEquipmentAssets.DYABLE_IRON);
+    public static final ArmorMaterial DYABLE_DIAMOND = createDyable(ArmorMaterials.DIAMOND, KCEquipmentAssets.DYABLE_DIAMOND);
+    public static final ArmorMaterial DYABLE_NETHERITE = createDyable(ArmorMaterials.NETHERITE, KCEquipmentAssets.DYABLE_NETHERITE);
+
+    // TODO: Create own sound and repair material tag!
+    public static final ArmorMaterial KING_ARMOR = new ArmorMaterial(35, ArmorMaterials.makeDefense(3, 6, 8, 3, 11), 20, SoundEvents.ARMOR_EQUIP_NETHERITE, 4.0F, 0.2F, ItemTags.REPAIRS_DIAMOND_ARMOR, KCEquipmentAssets.KING);
+
+    // TODO: Create own sound and repair material tag!
+    public static final ArmorMaterial CLOTH = new ArmorMaterial(4, ArmorMaterials.makeDefense(1, 2, 3, 1, 3), 25, Holder.direct(SoundEvents.WOOL_PLACE), 1.0F, 0.0F, ItemTags.WOOL, KCEquipmentAssets.CLOTH);
+
+    static ArmorMaterial createHeavy(ArmorMaterial baseMaterial, ResourceKey<EquipmentAsset> assetResourceKey) {
+        Map<ArmorType, Integer> defenses = Maps.newEnumMap(baseMaterial.defense());
+        defenses.entrySet().forEach(entry -> {
+            entry.setValue((int) Math.round(entry.getValue() * 1.25D));
+        });
+        return new ArmorMaterial((int) Math.round(baseMaterial.durability() * 1.5), defenses, baseMaterial.enchantmentValue(), baseMaterial.equipSound(), baseMaterial.toughness() * 1.5F, baseMaterial.knockbackResistance() + 0.1F, baseMaterial.repairIngredient(), assetResourceKey);
+    }
+
+    static ArmorMaterial createDyable(ArmorMaterial baseMaterial, ResourceKey<EquipmentAsset> assetResourceKey) {
+        Map<ArmorType, Integer> defenses = Maps.newEnumMap(baseMaterial.defense());
+        defenses.entrySet().forEach(entry -> {
+            entry.setValue((int) Math.round(entry.getValue() * 0.75D));
+        });
+        return new ArmorMaterial((int) Math.round(baseMaterial.durability() * 0.75D), defenses, baseMaterial.enchantmentValue(), baseMaterial.equipSound(), baseMaterial.toughness() * 0.75F, baseMaterial.knockbackResistance() * 0.75F, baseMaterial.repairIngredient(), assetResourceKey);
+    }
+
 }
--- src/main/java/de/dertoaster/kerkercraft/init/KCEquipmentAssets.java
+++ src/main/java/de/dertoaster/kerkercraft/init/KCEquipmentAssets.java
@@ -1,2 +1,33 @@
-package de.dertoaster.kerkercraft.init;public interface KCEquipmentAssets {
+package de.dertoaster.kerkercraft.init;
+
+import de.dertoaster.kerkercraft.Kerkercraft;
+import net.minecraft.core.Registry;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.world.item.equipment.EquipmentAsset;
+import net.minecraft.world.item.equipment.EquipmentAssets;
+
+public interface KCEquipmentAssets extends EquipmentAssets {
+
+    ResourceKey<? extends Registry<EquipmentAsset>> REGISTRY_ID = ResourceKey.createRegistryKey(Kerkercraft.prefix("equipment_asset"));
+
+    static ResourceKey<EquipmentAsset> register(String id) {
+        return ResourceKey.create(REGISTRY_ID, Kerkercraft.prefix("id"));
+    }
+
+    // TODO: Can we do it like this???
+
+
+    ResourceKey<EquipmentAsset> TORTOISE = register("tortoise");
+    ResourceKey<EquipmentAsset> BULL = register("bull");
+    ResourceKey<EquipmentAsset> SLIME = register("slime");
+    ResourceKey<EquipmentAsset> HEAVY_IRON = register("heavy_iron");
+    ResourceKey<EquipmentAsset> HEAVY_DIAMOND = register("heavy_diamond");
+    ResourceKey<EquipmentAsset> HEAVY_NETHERITE = register("heavy_netherite");
+    ResourceKey<EquipmentAsset> KING = register("king_armor");
+    ResourceKey<EquipmentAsset> DYABLE_GOLD = register("dyable_gold");
+    ResourceKey<EquipmentAsset> DYABLE_IRON = register("dyable_iron");
+    ResourceKey<EquipmentAsset> DYABLE_DIAMOND = register("dyable_diamond");
+    ResourceKey<EquipmentAsset> DYABLE_NETHERITE = register("dyable_netherite");
+    ResourceKey<EquipmentAsset> CLOTH = register("cloth");
+
 }
