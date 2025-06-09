67638665bcc267a2e76b7ec0dba87c874ab786ba
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2025-06-09 19:50:02 +0200

    add repair item tags for armor

--- src/main/java/de/dertoaster/kerkercraft/init/KCItemTags.java
+++ src/main/java/de/dertoaster/kerkercraft/init/KCItemTags.java
@@ -1,2 +1,19 @@
-package de.dertoaster.kerkercraft.init;public class KCItemTags {
+package de.dertoaster.kerkercraft.init;
+
+import de.dertoaster.kerkercraft.Kerkercraft;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.item.Item;
+
+public class KCItemTags {
+
+    public static final TagKey<Item> REPAIRS_BULL_ARMOR = create("repairs_bull_armor");
+    public static final TagKey<Item> REPAIRS_TORTOISE_ARMOR = create("repairs_tortoise_armor");
+    public static final TagKey<Item> REPAIRS_SLIME_ARMOR = create("repairs_slime_armor");
+
+    private static TagKey<Item> create(String id) {
+        return TagKey.create(Registries.ITEM, Kerkercraft.prefix(id));
+    }
+
 }
