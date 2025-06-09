36be1a9e91b79061c2d51d8c6917d90186be15a5
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2025-06-09 15:24:04 +0200

    rename class

--- src/main/java/de/dertoaster/kerkercraft/world/item/vanilla/MultiUsePotionItem.java
+++ src/main/java/de/dertoaster/kerkercraft/world/item/vanilla/MultiUsePotionItem.java
@@ -2,10 +2,11 @@ package de.dertoaster.kerkercraft.world.item.vanilla;
 
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.PotionItem;
 import net.minecraft.world.level.Level;
 
-public class PotionItem extends net.minecraft.world.item.PotionItem {
-    public PotionItem(Properties p_42979_) {
+public class MultiUsePotionItem extends PotionItem {
+    public MultiUsePotionItem(Properties p_42979_) {
         super(p_42979_);
     }
 
