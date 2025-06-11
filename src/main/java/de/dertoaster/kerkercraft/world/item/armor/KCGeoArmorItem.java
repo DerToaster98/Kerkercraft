1454cd6fccff4d947976e1fdd96694d94a88e63f
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2025-06-11 18:44:07 +0200

    implement createGeoRenderer => automatically creates the armorRenderer instance

--- src/main/java/de/dertoaster/kerkercraft/world/item/armor/KCGeoArmorItem.java
+++ src/main/java/de/dertoaster/kerkercraft/world/item/armor/KCGeoArmorItem.java
@@ -1,12 +1,22 @@
 package de.dertoaster.kerkercraft.world.item.armor;
 
+import de.dertoaster.kerkercraft.client.renderer.geo.armor.KCGeoArmorRenderer;
+import net.minecraft.client.model.HumanoidModel;
+import net.minecraft.client.renderer.entity.state.HumanoidRenderState;
+import net.minecraft.client.resources.model.EquipmentClientInfo;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.item.Item;
-import net.minecraft.world.item.equipment.ArmorMaterial;
-import net.minecraft.world.item.equipment.ArmorType;
+import net.minecraft.world.item.ItemStack;
+import org.jetbrains.annotations.Nullable;
 import software.bernie.geckolib.animatable.GeoItem;
+import software.bernie.geckolib.animatable.client.GeoRenderProvider;
 import software.bernie.geckolib.animatable.instance.AnimatableInstanceCache;
 import software.bernie.geckolib.animatable.manager.AnimatableManager;
+import software.bernie.geckolib.renderer.GeoArmorRenderer;
 import software.bernie.geckolib.util.GeckoLibUtil;
+
+import java.util.function.Consumer;
 
 public class KCGeoArmorItem extends Item implements GeoItem {
 
@@ -25,4 +35,21 @@ public class KCGeoArmorItem extends Item implements GeoItem {
     public AnimatableInstanceCache getAnimatableInstanceCache() {
         return cache;
     }
+
+    @Override
+    public void createGeoRenderer(Consumer<GeoRenderProvider> consumer) {
+        consumer.accept(new GeoRenderProvider() {
+            private KCGeoArmorRenderer<? extends KCGeoArmorItem, ?> renderer;
+
+            @Override
+            public @Nullable <S extends HumanoidRenderState> GeoArmorRenderer<?, ?> getGeoArmorRenderer(@Nullable S renderState, ItemStack itemStack, EquipmentSlot equipmentSlot, EquipmentClientInfo.LayerType type, @Nullable HumanoidModel<S> original) {
+                if (this.renderer == null) {
+                    // TODO: Is this safe to do?
+                    this.renderer = new KCGeoArmorRenderer<>(BuiltInRegistries.ITEM.getKey(itemStack.getItem()));
+                }
+                return this.renderer;
+            }
+        });
+    }
+
 }
